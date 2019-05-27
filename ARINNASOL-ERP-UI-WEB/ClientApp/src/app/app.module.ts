f05a314bd997e5b2cfee35c34c8ae521836cabc1
Jabran Shaheen <JabranShaheen@hotmail.com>
2019-05-27 21:35:02 +0100

    Latest

--- ARINNASOL-ERP-UI-WEB/ClientApp/src/app/app.module.ts
+++ ARINNASOL-ERP-UI-WEB/ClientApp/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -9,6 +8,28 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {MsalModule} from "@azure/msal-angular";
+import { MsalInterceptor} from "@azure/msal-angular";
+import {LogLevel} from "msal";
+
+import { HttpServiceHelper } from './common/HttpServiceHelper';
+
+export function loggerCallback(logLevel, message, piiEnabled) {
+  console.log("client logging" + message);
+}
+
+export const protectedResourceMap:[
+  string, 
+  string[]][]=[ 
+    ['https://arinnasol-erp-bll-api.azurewebsites.net/api/values',
+    ['https://arinnasol-erp-bll-api.azurewebsites.net/api/values/user_impersonation']] , 
+    ['https://graph.microsoft.com/v1.0/me', 
+    ['user.read']],
+    ['https://graph.microsoft.com/Group.Read.All', 
+    ['Directory.Read.All']] 
+  ];
 
 @NgModule({
   declarations: [
@@ -26,9 +47,31 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    ]),
+    MsalModule.forRoot({
+        clientID: 'cf986618-5fbd-4f52-869e-af4e55a15d7f',
+        authority: "https://login.microsoftonline.com/bd4dc115-f16f-425b-943b-4c660006109c",
+        validateAuthority: true,
+        // redirectUri: "https://arinnasol-erp-ui.azurewebsites.net",
+        redirectUri: "http://localhost:4200",
+        cacheLocation : "localStorage",
+        // postLogoutRedirectUri: "https://arinnasol-erp-ui.azurewebsites.net",
+        postLogoutRedirectUri: "http://localhost:4200",        
+        navigateToLoginRequestUrl: true,
+        popUp: false,
+        consentScopes: ["Directory.Read.All", "user.read", "https://arinnasol-erp-bll-api.azurewebsites.net/api/values/user_impersonation"],
+        unprotectedResources: ["https://www.microsoft.com/en-us/"],
+        protectedResourceMap: protectedResourceMap,
+        logger: loggerCallback,
+        correlationId: '6666',
+        level: LogLevel.Info,
+        piiLoggingEnabled: true         
+      }
+    )
   ],
-  providers: [],
+  providers: [ HttpServiceHelper,
+     {provide: HTTP_INTERCEPTORS, useClass: MsalInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ARINNASOL-ERP-UI-WEB/ClientApp/src/app/home/home.component.ts
+++ ARINNASOL-ERP-UI-WEB/ClientApp/src/app/home/home.component.ts
@@ -1,8 +1,114 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from "rxjs/Subscription";
+import {BroadcastService} from "@azure/msal-angular";
+import { MsalService} from "@azure/msal-angular";
+import {Injectable} from "@angular/core";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {Observable} from "rxjs/Rx";
+import { HttpServiceHelper } from '../common/HttpServiceHelper';
 
-@Component({
+
+@Component
+(
+  {
   selector: 'app-home',
   templateUrl: './home.component.html',
-})
-export class HomeComponent {
+  }
+)
+export class HomeComponent 
+{
+
+  constructor (
+                private http: HttpClient, 
+                private authService : MsalService,
+                private httpService: HttpServiceHelper, 
+                private broadcastService: BroadcastService                
+              ) 
+  {
+
+  }
+  private apiEndpoint: string = "https://arinnasol-erp-bll-api.azurewebsites.net/api/values";
+
+  APICall()
+  {
+      this.http.get(this.apiEndpoint)
+      .subscribe
+      (
+        (result) => console.log(result)
+      );
+    return null;  
+  }
+
+  login()
+  {
+    this.authService.loginPopup(
+                                [
+                                  "user.read" 
+                                  ,
+                                  "https://arinnasol-erp-bll-api.azurewebsites.net/api/values/user_impersonation"
+                                ]
+                              );
+  }
+
+  GetUserProfile()
+  {
+    let userData:any;
+    let url = "https://graph.microsoft.com/v1.0/me";
+
+    this.httpService.httpGetRequest(url)
+    .subscribe(data => 
+    {
+      console.log("User Data");
+      userData = data;
+      console.log(userData);
+    }, error => 
+      {
+        console.error(" Http get request to MS Graph failed" + JSON.stringify(error));
+      }
+    );
+    
+    
+
+  }
+
+  GetGroups()
+  {
+  
+      let userData:any;
+      let url = "https://graph.microsoft.com/v1.0/me/memberOf";
+
+      this.httpService.httpGetRequest(url)
+      .subscribe(data => 
+      {
+        console.log("Group Data");
+        userData = data;
+        console.log(userData);
+      }, error => 
+        {
+          console.error(" Http get request to MS Graph failed" + JSON.stringify(error));
+        }
+      );
+  
+
+
+  }
+
+
+  // CreateUser()
+  // {
+  
+  //     let userData:any;
+  //     let url = "https://graph.microsoft.com/v1.0/users";
+
+  //     this.http.post(this.apiEndpoint,)
+  //     .subscribe
+  //     (
+  //       (result) => console.log(result)
+  //     );
+  //   return null;    
+
+
+  // }
+
+
 }
